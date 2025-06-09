864a08ce75bbeb511a79be1b0c1c4387e2a3e5ac
Edinson Rivera <edirju@gmail.com>
2025-06-09 16:39:12 -0500

    Iniciar Repositorio + carga de archivos iniciales

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), preact()],
+    vite: {
+    plugins: [tailwindcss()],
+  },
 });
---
+++ src/data/consts.js
@@ -0,0 +1,256 @@
+export const PROJECTS = [
+  {
+    slug: "01-tinder-swipe",
+    title: "Tinder Swipe",
+    hidden: false,
+    description:
+      "Aprende a crear el efecto de swipe de Tinder con HTML, CSS y JavaScript.",
+    learnings: [
+      "DOM",
+      "Animaciones con gestos",
+      "Eventos de mouse y touch",
+      "Transiciones CSS",
+    ],
+    youtube: "https://www.youtube.com/watch?v=u01WD_YNENY",
+  },
+  {
+    slug: "02-music-player",
+    title: "Simple Music Player",
+    description: "Aplicación que permite la reproducción de audios.",
+    learnings: [
+      "Loop",
+      "Eventos de teclado",
+      "RequestAnimationFrame",
+      "Sprites",
+    ],
+    youtube: "#",
+  },
+  {
+    slug: "03-game-css-animations",
+    title: "Bunny - Juego con CSS",
+    description: "Animaciones con gestos y CSS.",
+    learnings: ["Transiciones CSS", "Eventos de mouse", "Sprites"],
+    youtube: "#",
+  },
+  {
+    slug: "04-pomodoro-app",
+    title: "Pomodoro App",
+    description: "Aplicacion de Pomodoro, temporizadores de tiempo.",
+    learnings: ["Métodos", "Eventos de teclado", "Sprites"],
+    youtube: "#",
+  },
+  {
+    slug: "05-typing-game",
+    title: "Reto de Mecanografía",
+    description: "Pon a prueba tus habilidades de mecanografía con este reto",
+    learnings: [
+      "DOM",
+      "Eventos de teclado",
+      "Manejo de clases",
+      "Optimización selectores",
+    ],
+    theme: {
+      isDark: true,
+    },
+    youtube: "https://www.youtube.com/watch?v=157qVlTelOg",
+  },
+  {
+    slug: "06-pixel-art-generator",
+    title: "Pixel Art Generator",
+    description:
+      "Aplicación clásica para dibujar píxeles con un color y un tamaño",
+    learnings: ["Dibujar en Canvas", "Function", "Eventos de teclado", "Touch"],
+    youtube: "https://www.youtube.com/watch?v=b6du6MvQmuQ",
+  },
+  {
+    slug: "07-rich-text-editor",
+    title: "Editor Online",
+    description: "Editor similar a Word, pero más pequeño",
+    learnings: ["Custom CSS", "Functions", "ESModules"],
+    theme: {
+      isDark: false,
+    },
+     youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "08-password-generator",
+    title: "Crea contraseñas",
+    description: "Generador de contraseñas de forma segura",
+    learnings: ["Custom CSS", "Functions", "ESModules"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "09-qr-code-generator",
+    title: "Crea códigos QR",
+    description: "Generador de códigos QR en JS",
+    learnings: ["Custom CSS", "Functions", "ESModules"],
+    theme: {
+      isDark: true,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "10-adivinanza-juego",
+    title: "Adivina el número",
+    description: "Juego que nos permite adivinar un número",
+    learnings: ["Custom CSS", "Funciones", "Condicionales"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "11-calculadora-v1",
+    title: "Calculadora JS",
+    description: "calculadora con funciones aritméticas básicas.",
+    learnings: ["Custom CSS", "Funciones", "Condicionales"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "12-movie-seat-booking",
+    title: "Reserva de asientos cine",
+    description:
+      "Simulador que permite asignar asientos de cine a los usuarios.",
+    learnings: ["Custom CSS", "Funciones", "Condicionales"],
+    theme: {
+      isDark: true,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "13-exchange-rate",
+    title: "Conversor de monedas",
+    description:
+      "Aplicacion que nos permite calcular el valor de una moneda en otra.",
+    learnings: ["Custom CSS", "Funciones", "Condicionales"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "14-hangman",
+    title: "Juego El Ahorcado",
+    description:
+      "Aplicacion que nos permite calcular el valor de una moneda en otra.",
+    learnings: ["Custom CSS", "Funciones", "Condicionales"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "15-arkanoid-game",
+    title: "Arkanoid",
+    description:
+      "Juego mítico y clásico de Arkanoid para controlar con teclado.",
+    learnings: [
+      "Dibujar en Canvas",
+      "Eventos de teclado",
+      "RequestAnimationFrame",
+      "Sprites",
+    ],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "16-breakout-game",
+    title: "Breakout",
+    description:
+      "Juego clásico de Breakout para controlar con teclado y mouse.",
+    learnings: ["Dibujar en Canvas", "Eventos de teclado", "Clases"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "17-chatgpt-local",
+    title: "ChatGPT local",
+    description: "Usa IA de forma local y gratis. 100% privacidad.",
+    learnings: ["Web Workers", "IA", "ESModules"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "18-efectos-css",
+    title: "Efectos CSS",
+    description: "Espectaculares efectos en CSS para tus proyectos.",
+    learnings: ["animaciones", "CSS", "Clases"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  // {
+  //   slug: "19-css-hover",
+  //   title: "CSS Hover + Animación",
+  //   description: "Efecto CSS hover con animaciones para tus proyectos.",
+  //   learnings: ["animaciones", "CSS", "Clases", "hover"],
+  //   theme: {
+  //     isDark: false,
+  //   },
+  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  // },
+  // {
+  //   slug: "20-css-border-animation",
+  //   title: "CSS Border + Animación",
+  //   description: "Efecto CSS hover con animaciones para tus proyectos.",
+  //   learnings: ["animaciones", "CSS", "Clases", "hover"],
+  //   theme: {
+  //     isDark: false,
+  //   },
+  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  // },
+  // {
+  //   slug: "21-css-text-animated",
+  //   title: "CSS Border + Animación",
+  //   description: "Efecto CSS hover con animaciones para tus proyectos.",
+  //   learnings: ["animaciones", "CSS", "Clases", "hover"],
+  //   theme: {
+  //     isDark: false,
+  //   },
+  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  // },
+  // {
+  //   slug: "08-tetris-canvas",
+  //   title: "Tetris en Canvas",
+  //   description:21-css-text-animated
+  //     "Resolvemos una prueba técnica que te propone crear el Tetris en 40 minutos",
+  //   learnings: ["Canvas", "Eventos de teclado", "Lógica de programación"],
+  //   theme: {
+  //     isDark: true,
+  //   },
+  //   youtube: "https://www.youtube.com/watch?v=pNiyz0sl1no",
+  // },
+  // {
+  //   slug: "09-tier-maker",
+  //   title: "Tier Maker",
+  //   description:
+  //     "Arrastra y suelta las imágenes para crear tus propias listas de niveles",
+  //   learnings: ["Drag & Drop", "Input de imágenes", "CSS Custom Properties"],
+  //   theme: {
+  //     isDark: true,
+  //   },
+  //   youtube: "https://www.youtube.com/watch?v=LPzG0PnOzgA",
+  // },
+  // {
+  //   slug: "10-excel-js",
+  //   title: "Hojas de cálculo",
+  //   description: "Crea tu propio Excel sin dependencias y funcional",
+  //   learnings: ["Tablas", "Eventos de Input: focus y blur", "Eval"],
+  //   theme: {
+  //     isDark: false,
+  //   },
+  // },
+];
